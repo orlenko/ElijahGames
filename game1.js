53769c54e2c2867a5989432ecce05378f6e7fcd5
Volodymyr Orlenko <orlenko@gmail.com>
2016-02-13 22:13:47 -0800

    Update game1.js

--- game1.js
+++ game1.js
@@ -1,13 +1,15 @@
 
 var x1 =100;
-var y1 =20;
+var y1 =50;
 var x2 =300;
-var y2 =50;
+var y2 =150;
 var x3 =100;
-var y3 = 80;
+var y3 = 250;
 var x4 = 300;
-var y4 = 110;
-
+var y4 = 350;
+var monh =90;
+var monw =150;
+var speed =1;
 
 
 
@@ -15,10 +17,15 @@ draw = function() {
     
     background(5, 5, 5);
     fill(0, 221, 255);
-    ellipse(200, mouseY, 30, 30);
+    ellipse(200, mouseY, 50, 50);
     fill(255, 0, 0);
-    ellipse(x1,y1,150,10);
-    ellipse(x2,y2,150,10);
-    ellipse(x3,y3,150,10);
-    ellipse(x4,y4,150,10);
+    ellipse(x1,y1,monw,monh);
+    ellipse(x2,y2,monw,monh);
+    ellipse(x3,y3,monw,monh);
+    ellipse(x4,y4,monw,monh);
+ x1+=speed;
+ x2+=speed;
+ x3+=speed;
+ x4+=speed;
+ if (
 };
